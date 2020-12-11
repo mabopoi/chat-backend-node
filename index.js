bde68155a5f32cf7a4bc378c97fb4b7e2e784a73
unknown <mabopoi@gmail.com>
2020-12-11 02:27:35 -0300

    Socket working

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.get('/', (req, res) => {
   res.json({ test: "It's working" });
 });
 
-app.listen('3000');
+io.on('connection', (socket) => {
+  io.emit('connection', 'Someone has connected to the chat');
+
+  socket.on('chat msg', (msg) => {
+    io.emit('chat msg', msg);
+  });
+  socket.on('disconnect', () => {
+    io.emit('disconnect', 'Someone has left the chat');
+  });
+});
+
+http.listen(3000, () => console.log('Server working'));
